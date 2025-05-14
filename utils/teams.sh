e261c4be3afd445927206408025fd0936dc17dfc
Joey Hafner <joey@jafner.net>
2025-05-14 16:48:07 -0700

    Add utility scripts for quickly running teams or zoom, which require allowing unfree software.

---
+++ utils/teams.sh
@@ -0,0 +1,1 @@
+export NIXPKGS_ALLOW_UNFREE=1; nix-shell -p teams-for-linux --impure --run 'teams-for-linux'
---
+++ utils/zoom.sh
@@ -0,0 +1,1 @@
+export NIXPKGS_ALLOW_UNFREE=1; nix-shell -p zoom-us --impure --run 'zoom-us'
