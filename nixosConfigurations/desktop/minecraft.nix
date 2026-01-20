b3d6048c68018e1e9f626bb002674a424115ba56
Joey Hafner <joey@jafner.net>
2026-01-20 13:51:05 -0800

    Switch back to 3050; sob loudly; add custom Prismlauncher package with temurin jre-17

--- nixosConfigurations/desktop/minecraft.nix
+++ nixosConfigurations/desktop/minecraft.nix
@@ -1,11 +1,15 @@
-{
-  pkgs,
-  username,
-  ...
-}: {
-  home-manager.users.${username} = {
-    home.packages = with pkgs; [
-      prismlauncher
-    ];
-  };
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    (prismlauncher.override {
+      # Add binary required by some mod
+      additionalPrograms = [ffmpeg];
+
+      jdks = [
+        jdk21
+        jdk17
+        jdk8
+        javaPackages.compiler.temurin-bin.jre-17
+      ];
+    })
+  ];
 }
